a07ad86b71715acce4f28c719a50f98dc5a723b8
gitmrostrowski <mr.ostrowski@gmail.com>
2026-02-08 23:05:07 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
+import Header from "@/components/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl">
       <body>
         <AuthProvider>
+          <Header />
           {children}
         </AuthProvider>
       </body>
