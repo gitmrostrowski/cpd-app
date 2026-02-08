3d6200349b9ad5485f8fac4b023461da62fb8dd9
gitmrostrowski <mr.ostrowski@gmail.com>
2026-02-08 18:33:38 +0100

    Replace createSupabaseServerClient with supabaseServer

--- app/portfolio/actions.ts
+++ app/portfolio/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { createSupabaseServerClient } from "@/lib/supabase/server";
+import { supabaseServer } from "@/lib/supabase/server";
 
 export async function addActivity(formData: FormData) {
   const supabase = createSupabaseServerClient();
