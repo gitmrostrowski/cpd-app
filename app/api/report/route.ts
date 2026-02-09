096fe0c54d5783e26b7b7e1120a27c2ad2366fdd
gitmrostrowski <mr.ostrowski@gmail.com>
2026-02-09 19:08:08 +0100

    Update route.ts

--- app/api/report/route.ts
+++ app/api/report/route.ts
@@ -1,1 +1,6 @@
-
+// app/report/route.ts
+export async function GET() {
+  return new Response("<!doctype html><html><body>Report OK</body></html>", {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+}
