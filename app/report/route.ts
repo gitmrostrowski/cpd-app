0b05fcb47818333ade5f5dcfdc95c2519d688d7d
gitmrostrowski <mr.ostrowski@gmail.com>
2026-02-09 19:08:29 +0100

    Delete app/report/route.ts

--- app/report/route.ts
+++
@@ -1,6 +0,0 @@
-// app/report/route.ts
-export async function GET() {
-  return new Response("<!doctype html><html><body>Report OK</body></html>", {
-    headers: { "content-type": "text/html; charset=utf-8" },
-  });
-}
