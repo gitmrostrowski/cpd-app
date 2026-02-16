e7d1fb94e934457d5784f6ef51e042d69fe37bc2
gitmrostrowski <mr.ostrowski@gmail.com>
2026-02-17 00:25:31 +0100

    Add Footer and restructure layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl">
       <body>
         <AuthProvider>
-          <Header />
-          {children}
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </AuthProvider>
       </body>
     </html>
