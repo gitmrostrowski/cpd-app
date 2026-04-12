93f594e673ee4938375fd408f38f5ab27376a251
gitmrostrowski <mr.ostrowski@gmail.com>
2026-04-13 00:39:33 +0200

    Create user report page component

---
+++ app/raporty/uzytkownik/page.tsx
@@ -0,0 +1,10 @@
+// app/raporty/uzytkownik/page.tsx
+import RaportUserClient from "./RaportUserClient";
+
+export const metadata = {
+  title: "Raport użytkownika | CRPE",
+};
+
+export default function RaportUserPage() {
+  return <RaportUserClient />;
+}
